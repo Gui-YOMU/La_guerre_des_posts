6b0198274a71e68129ed2c9ac146ab2f067e8c70
= <=>
2026-02-02 17:26:06 +0100

    Ajout du drag and drop

--- public/scripts/ticketView.js
+++ public/scripts/ticketView.js
@@ -1,6 +1,8 @@
 const todo = document.querySelector("#todo");
 const ongoing = document.querySelector("#ongoing");
 const done = document.querySelector("#done");
+const drops = document.querySelectorAll(".drop");
+let dragged;
 
 async function getTickets() {
     const response = await fetch("http://localhost:3000/tickets", {
@@ -9,13 +11,12 @@ async function getTickets() {
             "Content-type": "application/json"
         }
     });
-    const ticketsList = await response.json();    
+    const ticketsList = await response.json();
     return ticketsList;
 };
 
 async function displayTickets() {
     let ticketsList = await getTickets();
-    console.log(ticketsList);
     todo.replaceChildren();
     ongoing.replaceChildren();
     done.replaceChildren();
@@ -25,12 +26,16 @@ async function displayTickets() {
     ticketsList.forEach(ticket => {
         draggable = document.createElement("div");
         draggable.setAttribute("draggable", "true");
+        draggable.setAttribute("id", ticket._id);
         title = document.createElement("h4");
         title.textContent = ticket.title;
         content = document.createElement("p");
         content.textContent = ticket.content;
         draggable.appendChild(title);
         draggable.appendChild(content);
+        draggable.addEventListener("dragstart", (e) => {
+            dragged = e.target;
+        })
         switch (ticket.category) {
             case "todo": {
                 todo.appendChild(draggable);
@@ -49,3 +54,26 @@ async function displayTickets() {
 };
 
 displayTickets();
+
+async function updateTicketCategory(id, category) {
+    const response = await fetch(`http://localhost:3000/tickets/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify({ category }),
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+    return response;
+}
+
+drops.forEach(drop => {
+    drop.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+    drop.addEventListener("drop", (e) => {
+        e.preventDefault();
+        updateTicketCategory(dragged.id, drop.id)
+        dragged.parentNode.removeChild(dragged);
+        e.target.appendChild(dragged);
+    })
+})
