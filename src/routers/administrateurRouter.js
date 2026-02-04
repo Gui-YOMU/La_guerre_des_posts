925ba4c81f4469463db3203f06d8af4328a0addf
Christel <reche.christel@]git config --global user.email reche.christel@gmail.com>
2026-02-04 17:19:25 +0100

    updates backend

--- src/routers/administrateurRouter.js
+++ src/routers/administrateurRouter.js
@@ -5,16 +5,12 @@ export const administrateurRouter = express.Router();
 
 // Admin CRUD tickets
 administrateurRouter.post("/tickets", adminController.createTicket);
-administrateurRouter.get("/tickets", adminController.listTickets);
-administrateurRouter.get("/tickets/:id", adminController.getTicketById);
 administrateurRouter.put("/tickets/:id", adminController.updateTicket);
 administrateurRouter.delete("/tickets/:id", adminController.deleteTicket);
 
 // Admin actions
-administrateurRouter.patch("/tickets/:id/assign", adminController.assignEmployee);
-administrateurRouter.patch("/tickets/:id/status", adminController.updateStatus);
-
-
-
-
-
+administrateurRouter.patch(
+  "/tickets/:id/assign",
+  adminController.assignEmployee,
+);
+//administrateurRouter.patch("/tickets/:id/status", adminController.updateStatus);
